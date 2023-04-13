1f74f192a54aa5477bc51538383d6a7e7cbef87d
stotzbalazs <70143048+stotzbalazs@users.noreply.github.com>
2023-04-13 16:23:30 +0200

    Initial

---
+++ gs-custom-settings.php
@@ -0,0 +1,25 @@
+<?php
+/*
+* Plugin Name: GreenShift - Custom Settings
+* Plugin URI: #
+* Description: Custom settings for GreenShift theme.
+* Version: 1.0
+* Author: Jedigital
+* Author URI: https://jedigital.net/
+* License: GPLv2 or later
+* Text Domain: greenshift
+*/
+
+if (!defined('ABSPATH'))
+    exit;
+
+//ACF Google Maps API
+function my_acf_google_map_api($api)
+{
+
+    $api['key'] = 'AIzaSyCx33WC9R-IRNuFxIiiSQ_7OchIZs7Vcvo';
+
+    return $api;
+}
+
+add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
